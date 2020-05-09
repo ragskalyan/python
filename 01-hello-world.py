e4a907129299690553a545824e18fd49b508649e
Kalyan C <ragskalyan@outlook.com>
2020-05-09 18:48:02 +0530

    master

---
+++ 01-hello-world.py
@@ -0,0 +1,12 @@
+# 1st Program in Python
+
+#Method 1
+print ("Hello World")
+
+#Method 2
+print ('''Hello World,
+This is my first programming''')
+
+#Method 3
+
+print('Hello World,\nThis is my first programming')
---
+++ 02-datatypes.py
@@ -0,0 +1,46 @@
+# Data Type: String
+
+first_name = "Kalyan "
+last_name = "Chandramohan"
+full_name = first_name+last_name
+
+print ("Name: "+full_name)
+
+# Data Type: Number : INT
+
+a = 999
+b = 1
+c = a+b
+
+print("Sum of A + B "+ str(c))
+
+# Data Type: Number : Float
+
+d = 1.5
+e = 1
+f = d+e
+
+print (f)
+print ("The integer with decimal is "+str(type(f)))
+
+
+# Data Type: Number : Complex
+
+g = 1j
+
+print(type(g))
+
+# Data Type: Sequence Types: List
+
+fruits = ["apple", "Orange", "Grapes", "Kiwi", "Pine Apple", "Mango"]
+
+print(fruits[5])
+
+# List the fruits
+
+for x in fruits:
+    print (x)
+
+# # Data Type: Sequence Types: Tuple
+
+git 
