838941b32b3247682302790b3f1511455d9286a2
Kalyan C <ragskalyan@outlook.com>
2020-05-24 13:04:10 +0530

    master

--- Fun-Programs/HungryDog.py
+++ Fun-Programs/HungryDog.py
@@ -8,14 +8,14 @@ Dog_Name, Cat_Name, Boss_Name = 'Julie','ChowZi','Sam'
 #Cat_Name = 'ChowZi'
 #Boss_Name = 'Sam'
 '''Here, I have used String,lists, tuples & dictionary data types & len function and also three types of print with f-string method & concordinates'''
-print (f"{Dog_Name}: Hello Boss, I am feeling hungry....Wakeup")
-print (f"{Boss_Name}: Hello {Dog_Name} & {Cat_Name}, Good Morning")
-print (f'''{Boss_Name}: Lemme See, {Dog_Name}''')
-print (f'{Dog_Name}: Fast fast....\n\tI have eat something now...\n{Boss_Name}: Wait dear {Dog_Name} let me check\n{Cat_Name}: Meow....\n{Boss_Name}: Okay...{Cat_Name} you also wakeup now')
+print ("{}: Hello Boss, I am feeling hungry....Wakeup".format(Dog_Name))
+print ("{0}: Hello {1} & {2}, Good Morning".format(Boss_Name,Dog_Name,Cat_Name))
+print ('''{0}: Lemme See, {1}'''.format(Boss_Name,Dog_Name))
+print ('{0}: Fast fast....\n\tI have eat something now...\n{1}: Wait dear {2} let me check\n{3}: Meow....\n{4}: Okay...{5} you also wakeup now'.format(Dog_Name,Boss_Name,Dog_Name,Cat_Name,Boss_Name,Cat_Name))
 
 # List & Ditionary
 foodlist = ['Bone', 'Bread', 'Egg', 'Juice', '{"Fruits": ["Oragne", "Apple", "Grapes", "Kiwi"]}', 'Chicken', 'Milk']
 Nooffood = len(foodlist)
 
-print (f'{Boss_Name}: Hey {Dog_Name}, There are total {Nooffood} food items we have \n{Dog_Name}: What we have? \n{Boss_Name}: We have ' + str(foodlist[0:3]) + ' and ' + str(foodlist[-2:-1]) + f'and for our {Cat_Name} we have ' + str(foodlist[-1]) +f'\n{Dog_Name}: Wow.... I need my fav '+ str(foodlist[0]))
-print (f'{Boss_Name}: Okay {Dog_Name} ' + '\n\tTake this ' + str(foodlist[0])+f'\n\tHey {Cat_Name}, Come and have ' + str(foodlist[-1]) + f'\n{Cat_Name}: Meow....')
+print (('{0}: Hey {1}, There are total {2} food items we have \n{3}: What we have? \n{4}: We have '.format(Boss_Name,Dog_Name,Nooffood,Dog_Name,Boss_Name)) + str(foodlist[0:3]) + ' and ' + str(foodlist[-2:-1]) + ('and for our {} we have '.format(Cat_Name)) + str(foodlist[-1]) +('\n{}: Wow.... I need my fav '.format(Dog_Name))+ str(foodlist[0]))
+print (('{0}: Okay {1} '.format(Boss_Name,Dog_Name)) + '\n\tTake this ' + str(foodlist[0])+('\n\tHey {}, Come and have '.format(Cat_Name)) + str(foodlist[-1]) + ('\n{}: Meow....'.format(Cat_Name)))
---
+++ Fun-Programs/usrpassd.py
@@ -0,0 +1,16 @@
+# Authentication Program
+import os
+import datetime
+
+print('''Welcome to the Authentication System\nPlz enter your credentials''')
+username = input('Username: ' )
+passwd = input('Password: ')
+
+if username == 'john' and passwd == 'john':
+    print('Logged in as {}'.format(username))
+elif username == 'sam' and passwd == 'sam':
+    print('Logged in as {}'.format(username))
+
+else:
+    print('Login Failed')
+
